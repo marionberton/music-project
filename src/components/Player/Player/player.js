c264e42281faa13ed1f0faa408b3368513decca5
glowkeeper <steve@glowkeeper.uk>
2020-06-05 17:40:48 +0100

    Plays all five tracks

--- src/components/Player/Player/player.js
+++ src/components/Player/Player/player.js
@@ -7,23 +7,26 @@ export const Player = ( props ) => {
   useEffect(() => {
 
       if (tracks.length) {
-          let deviceId = ''
+
           spotify.getMyDevices( function ( err, data ) {
               if (err) {
                   console.error(err)
               } else {
-                  console.log(data)
-                  deviceId = data.devices[0].id
-                  spotify.play({device_id: deviceId, uris: [tracks[0].uri]}, function ( err, data ) {
+
+                  const uris = tracks.map( ( track ) => {
+                     return track.uri
+                  })
+
+                 spotify.play({device_id: data.devices[0].id, uris: uris}, function ( err, data ) {
                       if (err) {
                           console.error(err)
                       } else {
-                          console.log(tracks[0].uri)
+                          console.log(tracks)
                       }
                   })
               }
-          })
-      }
-}, [spotify, tracks]);
-  return null;
-};
+         })
+     }
+  }, [spotify, tracks])
+  return null
+}
