1f0925ee09f7fe14a81a778c5f038ed59f474b19
“Marion <marionberton03@gmail.com>
2020-07-15 17:00:33 +0100

    updated icon

--- src/components/Player/Player/tracks.js
+++ src/components/Player/Player/tracks.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-import classes from "./PlayList.module.css";
-import { getRandom } from "../../../util/util";
+import classes from "./Player.module.css";
 
 const Tracks = ({ tracks }) => {
-  const maxRandom = 1000000;
-  const random = getRandom(0, maxRandom);
   
-console.log('Tracks',tracks)
   return tracks.map((track) => (
     <div className={classes.Cover}>
       <img key={track.id} src={track.album.images[0].url} alt="album artwork" />
